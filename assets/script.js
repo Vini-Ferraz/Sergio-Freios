f5118ecad3d722696f09d25bc845fc1268bd5429
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2023-10-31 13:02:55 -0300

    Updates

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
+/*Configuração da biblioteca para o uso das animações quando scrolla*/
 ScrollReveal({ reset: true });
-
 ScrollReveal().reveal('.show-side', 
 {
     duration: 1500,
     origin: 'left',
     distance: '75px'
 });
-
 ScrollReveal().reveal('.show-up', 
 {
     duration: 1500,
