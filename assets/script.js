b90c8e3d752d79963bb8d64a75cb7ba447e1d155
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2023-10-31 00:38:33 -0300

    Updates

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('.show-side', 
+{
+    duration: 2000,
+    origin: 'left',
+    distance: '75px'
+});
+
+ScrollReveal().reveal('.show-up', 
+{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '75px'
+});
